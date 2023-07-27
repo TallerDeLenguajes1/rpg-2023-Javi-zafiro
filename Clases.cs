e2c93729941384f35e4c209af96ea02a6546a5be
Javi-zafiro <javialexiscortez2@gmail.com>
2023-07-27 12:20:27 -0300

    primera version funcional

--- Clases.cs
+++ Clases.cs
@@ -1,5 +1,9 @@
 namespace Clases;
+using usarApi;
+using System.IO;
+using System.Net;
 using System.Text.Json;
+
 public class Personaje
 {
     private string? tipo;
@@ -12,7 +16,7 @@ public class Personaje
     private int velocidad;
     private int nivel;
     private int armadura;
-    private int salud;
+    private double salud;
 
     public string? Tipo { get => tipo; set => tipo = value; }
     public string? Nombre { get => nombre; set => nombre = value; }
@@ -24,34 +28,93 @@ public class Personaje
     public int Velocidad { get => velocidad; set => velocidad = value; }
     public int Nivel { get => nivel; set => nivel = value; }
     public int Armadura { get => armadura; set => armadura = value; }
-    public int Salud { get => salud; set => salud = value; }
+    public double Salud { get => salud; set => salud = value; }
 }
 
-public class FabricaDePersonajes
+public static class FabricaDePersonajes
 {
-    private string[] tipos={"humano", "elfo", "mago", "troll", "vampiro", "hombre lobo"};
-    private string[] nombres={"Luca", "Javier", "Celeste", "Luciana", "Valentina", "Francisco"};
-
-    public Personaje crear(){
+    private static string[] tipos={"Danzante del filo", "Corredor del viento", "Rompedor del cielo", "Forjador de vinculos", "Escultor de voluntad", "Vigilante de la verdad", "Nominador de lo otro", "Tejedor de luz", "Custodio de piedra", "Portador de polvo"};
+    
+    public static  Personaje crear(string nombre, DateTime fec, int edad, string apodo){
         
         Personaje nuevo=new Personaje();
         int numero;
         numero=rdm(0,6);
         nuevo.Tipo=tipos[numero];
-        numero=rdm(0,6);
-        nuevo.Nombre=nombres[numero];
-        nuevo.Apodo="apodo"+numero;
-        nuevo.FechaNac=new DateTime(rdm(1723, 2006), rdm(1, 12), rdm(1,31));
-        nuevo.Edad = 2023 - nuevo.FechaNac.Year;
-        nuevo.Destreza=rdm(1, 6);
-        nuevo.Armadura=rdm(1,11);
-        nuevo.Velocidad=rdm(1,11);
-        nuevo.Fuerza=rdm(1,10);
-        nuevo.Nivel=rdm(1,10);
+        nuevo.Nombre=nombre;
+        nuevo.Apodo=apodo;
+        nuevo.FechaNac=fec;
+        nuevo.Edad = edad;
+        switch (nuevo.Tipo)
+        {
+            case "Dazante del filo":
+                nuevo.Destreza=5;
+                nuevo.Armadura=5;
+                nuevo.Velocidad=8;
+                nuevo.Fuerza=4;
+            break;
+            case "Corredor del viento":
+                nuevo.Destreza=4;
+                nuevo.Armadura=6;
+                nuevo.Velocidad=10;
+                nuevo.Fuerza=7;
+            break;
+            case "Rompedor del cielo":
+                nuevo.Destreza=4;
+                nuevo.Armadura=5;
+                nuevo.Velocidad=10;
+                nuevo.Fuerza=8;
+            break;
+            case "Forjador de vinculos":
+                nuevo.Destreza=2;
+                nuevo.Armadura=8;
+                nuevo.Velocidad=4;
+                nuevo.Fuerza=10;
+            break;
+            case "Escultor de voluntad":
+                nuevo.Destreza=2;
+                nuevo.Armadura=10;
+                nuevo.Velocidad=2;
+                nuevo.Fuerza=5;
+            break;
+            case "Vigilante de la verdad":
+                nuevo.Destreza=3;
+                nuevo.Armadura=8;
+                nuevo.Velocidad=7;
+                nuevo.Fuerza=3;
+            break;
+            case "Nominador de lo otro":
+                nuevo.Destreza=5;
+                nuevo.Armadura=6;
+                nuevo.Velocidad=6;
+                nuevo.Fuerza=9;
+            break;
+            case "Tejedor de luz":
+                nuevo.Destreza=4;
+                nuevo.Armadura=2;
+                nuevo.Velocidad=6;
+                nuevo.Fuerza=4;
+            break;
+            case "Custodio de piedra":
+                nuevo.Destreza=4;
+                nuevo.Armadura=10;
+                nuevo.Velocidad=5;
+                nuevo.Fuerza=8;
+            break;
+            case "Portador de polvo":
+                nuevo.Destreza=2;
+                nuevo.Armadura=5;
+                nuevo.Velocidad=9;
+                nuevo.Fuerza=9;
+            break;
+            default:
+            break;
+        }
+        nuevo.Nivel=rdm(1,6);
         nuevo.Salud=100;
         return nuevo;
     }
-    private int rdm(int min, int max){
+    private static int rdm(int min, int max){
         Random random = new Random();
         int numero;
         numero = random.Next(min, max);
@@ -59,16 +122,27 @@ public class FabricaDePersonajes
     }
 }
 
-public class persojanesJson
+public static class persojanesJson
 {
-    public void GuardarPersonaje(List<Personaje> lista, string nombre){
+    public static void GuardarPersonaje(List<Personaje> lista, string nombre){
         string rutaProyecto = Directory.GetCurrentDirectory();
         string rutaArchivo = Path.Combine(rutaProyecto, nombre);
-        string json=JsonSerializer.Serialize(lista, new JsonSerializerOptions{WriteIndented=true});
-        File.WriteAllText( rutaArchivo,json);
-        Console.WriteLine("se guardo la lista de personajes");
+        if (File.Exists(rutaArchivo))
+        {
+            string existente = File.ReadAllText(rutaArchivo);
+            List<Personaje> listaExistente = JsonSerializer.Deserialize<List<Personaje>>(existente);
+            List<Personaje> final = listaExistente.Concat(lista).ToList();
+            string json=JsonSerializer.Serialize(final, new JsonSerializerOptions{WriteIndented=true});
+            File.WriteAllText( rutaArchivo,json);
+        }else
+        {
+            string json=JsonSerializer.Serialize(lista, new JsonSerializerOptions{WriteIndented=true});
+            File.WriteAllText( rutaArchivo,json);
+            Console.WriteLine("se guardo la lista de personajes");
+        }
+        
     }
-    public List<Personaje> LeerPersonaje(string archivo){
+    public static List<Personaje> LeerPersonaje(string archivo){
         string rutaProyecto = Directory.GetCurrentDirectory();
         string rutaArchivo = Path.Combine(rutaProyecto, archivo);
         string json = File.ReadAllText(rutaArchivo);
@@ -84,9 +158,42 @@ public class persojanesJson
         
     }
 
-    public bool Existe(string archivo){
+    public static bool Existe(string archivo){
         string rutaProyecto = Directory.GetCurrentDirectory();
         string rutaArchivo = Path.Combine(rutaProyecto, archivo);
         return File.Exists(rutaArchivo);
+    }
+}
+
+public static class batalla
+{
+    public static double combate(Personaje num1, Personaje num2){
+        if (num2.Salud>=0)
+        {
+            int Ataque= num1.Destreza*num1.Fuerza*(num1.Nivel*2);
+            int Efectividad= rdm(30,101);
+            int Defenza=num2.Armadura*num2.Velocidad;
+            int constAjuste=500;
+            double Daño=((Ataque*Efectividad)-Defenza)/constAjuste;
+            return Daño;
+        }else
+        {
+            return 0;
+        }
+    }
+    public static int rdm(int min, int max){
+        Random random = new Random();
+        int numero;
+        numero = random.Next(min, max);
+        return numero;
+    }
+    public static void EscribirMensajePorLetras(string mensaje, TimeSpan tiempoDeEspera)
+    {
+        foreach (char letra in mensaje)
+        {
+            Console.Write(letra);
+            Thread.Sleep(tiempoDeEspera);
+        }
+        Console.WriteLine("");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,38 +1,193 @@
-﻿using Clases;
-List<Personaje> listaDePersonaje = new List<Personaje>();
-List<Personaje> listaObtenida = new List<Personaje>();
-Personaje nuevo;
-FabricaDePersonajes fb = new FabricaDePersonajes();
-int n=10;
-Console.WriteLine("ingrese la cantidad de personajes");
-int.TryParse(Console.ReadLine(), out n);
-for (int i = 0; i < n; i++)
-{
-    nuevo=fb.crear();
-    listaDePersonaje.Add(nuevo);
-}
-persojanesJson js = new persojanesJson(); 
-Console.WriteLine("ingrese el nombre del archivo");
-string? archivo = Console.ReadLine();
-js.GuardarPersonaje(listaDePersonaje, archivo);
+﻿
+using Clases;
+using usarApi;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+string? archivo = "Json";
+bool bandera;
+string? caracter="a";
+Console.Clear();
 
-bool existe = js.Existe(archivo);
+Console.WriteLine("|''||''|                                                                 '||'  '|'          ||    .   '||       ||                   ");
+Console.WriteLine("   ||      ...   ... ..  .. ...     ....    ...        ....  .. ...       ||    |  ... ..  ...  .||.   || ..   ...  ... ..  ... ...  ");
+Console.WriteLine("   ||    .|  '|.  ||' ''  ||  ||  .|...|| .|  '|.    .|...||  ||  ||      ||    |   ||' ''  ||   ||    ||' ||   ||   ||' ''  ||  ||  ");
+Console.WriteLine("   ||    ||   ||  ||      ||  ||  ||      ||   ||    ||       ||  ||      ||    |   ||      ||   ||    ||  ||   ||   ||      ||  ||  ");
+Console.WriteLine("  .||.    '|..|' .||.    .||. ||.  '|...'  '|..|'     '|...' .||. ||.      '|..'   .||.    .||.  '|.' .||. ||. .||. .||.     '|..'|. ");
+Console.WriteLine(" ");
+batalla.EscribirMensajePorLetras("Presione enter para continuar", TimeSpan.FromMilliseconds(30));
+Console.ReadKey();
+Console.Clear();
+
+batalla.EscribirMensajePorLetras("desea crear un nuevo personaje(aleatorio)? (Y/N)", TimeSpan.FromMilliseconds(30));
+do
+{
+    caracter=Console.ReadLine();
+    if (caracter!="y" && caracter!="Y" && caracter!="n" && caracter!="N")
+    {
+        batalla.EscribirMensajePorLetras("el caracter ingresado es incorrecto, intentelo de nuevo", TimeSpan.FromMilliseconds(30));
+        
+    }
+} while (caracter!="y" && caracter!="Y" && caracter!="n" && caracter!="N");
+
+if (caracter=="y" || caracter=="Y")
+{
+    string nombre;
+    DateTime fec;
+    int edad;
+    string apodo;
+    List<Personaje> agregar= new List<Personaje>();
+    Personaje nuevo;
+    var url = $"https://randomuser.me/api/";
+    var request = (HttpWebRequest)WebRequest.Create(url);
+    request.Method = "GET";
+    request.ContentType = "application/json";
+    request.Accept = "application/json";
+    try
+    {
+        using (WebResponse response = request.GetResponse())
+        {
+            using (Stream strReader = response.GetResponseStream())
+            {
+                if (strReader == null) return;
+                using (StreamReader objReader = new StreamReader(strReader))
+                {
+                    string responseBody = objReader.ReadToEnd();
+                    Root resultado = JsonSerializer.Deserialize<Root>(responseBody);
+                    nombre=resultado.results[0].name.first;
+                    fec=resultado.results[0].dob.date;
+                    edad=resultado.results[0].dob.age;
+                    apodo=resultado.results[0].login.username;
+                    nuevo=FabricaDePersonajes.crear(nombre, fec, edad, apodo);
+                    agregar.Add(nuevo);
+                    persojanesJson.GuardarPersonaje(agregar, archivo);
+                    Console.Clear();
+                    batalla.EscribirMensajePorLetras("El personaje fue creado! veamos sus datos", TimeSpan.FromMilliseconds(30));
+                    batalla.EscribirMensajePorLetras("Nombre: "+nuevo.Nombre, TimeSpan.FromMilliseconds(30));
+                    batalla.EscribirMensajePorLetras("Edad: "+nuevo.Edad, TimeSpan.FromMilliseconds(30));
+                    batalla.EscribirMensajePorLetras("Apodo: "+nuevo.Apodo, TimeSpan.FromMilliseconds(30));
+                    batalla.EscribirMensajePorLetras("Tipo: "+nuevo.Tipo, TimeSpan.FromMilliseconds(30));
+                    batalla.EscribirMensajePorLetras("Nivel: "+nuevo.Nivel, TimeSpan.FromMilliseconds(30));
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+    catch (WebException ex)
+    {
+        Console.WriteLine("Problemas de acceso a la API");
+    }
+    Console.Clear();
+    batalla.EscribirMensajePorLetras("Desea juagar con el personaje creado? (Y/N)", TimeSpan.FromMilliseconds(30));
+    batalla.EscribirMensajePorLetras("Tenga en cuenta lo siguiente:", TimeSpan.FromMilliseconds(30));
+    batalla.EscribirMensajePorLetras("- Su contrincante sera seleccionado de manera aleatoria", TimeSpan.FromMilliseconds(30));
+    batalla.EscribirMensajePorLetras("- Si no desea jugar con el personaje que se creo, su personaje sera elegido aleatoriamente", TimeSpan.FromMilliseconds(30));
+    do
+    {
+        caracter=Console.ReadLine();
+        if (caracter!="y" && caracter!="Y" && caracter!="n" && caracter!="N")
+        {
+            batalla.EscribirMensajePorLetras("el caracter ingresado es incorrecto, intentelo de nuevo", TimeSpan.FromMilliseconds(30));
+            
+        }
+    } while (caracter!="y" && caracter!="Y" && caracter!="n" && caracter!="N");
+    if (caracter=="y" || caracter=="Y")
+    {
+        bandera=true;
+    }else
+    {
+        bandera=false;
+    }
+}else
+{
+    Console.Clear();
+    batalla.EscribirMensajePorLetras("Como no desea crear nuevos personajes usaremos los ya existentes", TimeSpan.FromMilliseconds(30));
+    Console.ReadKey();
+    bandera=false;
+}
+
+
+
+List<Personaje> listaObtenida = new List<Personaje>();
+
+bool existe = persojanesJson.Existe(archivo);
 if (existe)
 {
-    listaObtenida=js.LeerPersonaje(archivo);
-    foreach (var item in listaObtenida)
+    listaObtenida=persojanesJson.LeerPersonaje(archivo);
+    int n=listaObtenida.Count;
+    int x;
+    int y;
+    int i=1;
+    bool cambio=true;
+    double daño;
+    if (bandera)
     {
-        Console.WriteLine("nombre:"+item.Nombre);
-        Console.WriteLine("apodo:"+item.Apodo);
-        Console.WriteLine("nivel:"+item.Nivel);
-        Console.WriteLine("Fecha de Nacimiento"+item.FechaNac);
-        Console.WriteLine("Salud"+item.Salud);
-        Console.WriteLine("Fuerza"+item.Fuerza);
-        Console.WriteLine("Edad"+item.Edad);
-        Console.WriteLine("Destreza"+item.Destreza);
-        Console.WriteLine("Armadura"+item.Armadura);
-        Console.WriteLine("===========================");
+        x=n-1;
+    }else
+    {
+        x=batalla.rdm(0, n);
     }
+    do
+    {
+        y=batalla.rdm(0,n);
+        while (y==x)
+        {
+            y=batalla.rdm(0, n);
+        }
+        
+        
+        
+        Console.WriteLine("Comencemos el "+i+"° combate");
+        Console.WriteLine("El primer combatiente: ");
+        Console.WriteLine("Nombre: "+ listaObtenida[x].Nombre);
+        Console.WriteLine("Apodo: "+ listaObtenida[x].Apodo);
+        Console.WriteLine("Nivel: "+ listaObtenida[x].Nivel);
+        Console.WriteLine("Tipo: "+ listaObtenida[x].Tipo);
+        Console.WriteLine("El segundo combatiente: ");
+        Console.WriteLine("Nombre: "+ listaObtenida[y].Nombre);
+        Console.WriteLine("Apodo: "+ listaObtenida[y].Apodo);
+        Console.WriteLine("Nivel: "+ listaObtenida[y].Nivel);
+        Console.WriteLine("Tipo: "+ listaObtenida[y].Tipo);
+        do
+        {
+            if (cambio)
+            {
+                daño=batalla.combate(listaObtenida[x], listaObtenida[y]);
+                Console.WriteLine("daño causado por el primer combatiente: "+daño);
+                listaObtenida[y].Salud-=daño;
+                cambio=false;
+            }else
+            {
+                daño=batalla.combate(listaObtenida[y], listaObtenida[x]);
+                Console.WriteLine("daño causado por el segundo combatiente: "+daño);
+                listaObtenida[x].Salud-=daño;
+                cambio=true;
+            }
+            
+            Console.WriteLine("salud del primer combatiente: "+listaObtenida[x].Salud);
+            Console.WriteLine("salud del segundo combatiente: "+listaObtenida[y].Salud);
+            Console.WriteLine("==========fin de round==========");
+            if (listaObtenida[x].Salud<=0)
+            {
+                Console.WriteLine("FUISTE VENCIDO");
+            }
+            if (listaObtenida[y].Salud<=0)
+            {
+                Console.WriteLine("VENCISTE A TU CONTRINCATE");
+            }
+            Console.WriteLine("...enter para continuar...");
+            Console.ReadLine();
+        } while (listaObtenida[x].Salud>0 && listaObtenida[y].Salud>0); 
+        i++;
+        if (listaObtenida[x].Salud>0)
+        {
+            listaObtenida[x].Salud=100;
+            if (listaObtenida[x].Nivel<10)
+            {
+                listaObtenida[x].Nivel++;
+            }
+        }
+    } while (listaObtenida[x].Salud>0 && i<n);
 }else
 {
     Console.Write("el archivo no existe");
